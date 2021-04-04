166661d839bd74c8f13052904c9f8aa914489ce5
Anthony H <anthony@hevia.dev>
2021-04-04 15:57:10 -0400

    Make coref a neat function call

--- tools/preprocess_data/coref_resolution.py
+++ tools/preprocess_data/coref_resolution.py
@@ -1,15 +1,22 @@
-# Load your usual SpaCy model (one of SpaCy English models)
 import spacy
-nlp = spacy.load('en_core_web_sm')
-
-# load NeuralCoref and add it to the pipe of SpaCy's model
+from typing import List
 import neuralcoref
-coref = neuralcoref.NeuralCoref(nlp.vocab)
-nlp.add_pipe(coref, name='neuralcoref')
 
-# You're done. You can now use NeuralCoref the same way you usually manipulate a SpaCy document and it's annotations.
-doc = nlp(u'My sister has a dog. She loves him.')
 
-print(f"doc._.has_coref: {doc._.has_coref}")
-print(f"doc._.coref_clusters: {doc._.coref_clusters}")
-print(f"doc._.coref_resolved: {doc._.coref_resolved}") # For the basic example, this prints: My sister has a dog. My sister loves a dog.
+def coref_resolution(sentences: List[str]) -> List[str]:
+    nlp = spacy.load('en_core_web_sm')
+    coref = neuralcoref.NeuralCoref(nlp.vocab)
+    nlp.add_pipe(coref, name='neuralcoref')
+
+    coref_sentences = []
+    for sentence in sentences:
+        doc = nlp(sentence)
+        if doc._.has_coref:
+            coref_sentences.append(doc._.coref_resolved)
+        else:
+            coref_sentences.append(sentence)
+    
+    return coref_sentences
+
+A simple test
+# print(coref_resolution(["My sister has a dog. My sister loves a dog.", "My sister has a dog. She loves him. "]))
