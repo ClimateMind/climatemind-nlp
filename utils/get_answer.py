91d20fa635044d214bce4b12e6bcbea3cbe6808d
Elle D <edashfield@gmail.com>
2021-06-06 09:20:18 +1200

    Added some error messages with Kameron's help.

--- utils/get_answer.py
+++ utils/get_answer.py
@@ -1,21 +1,29 @@
 import srsly
 import json
 
-file_name = "test-team-answers-bases-checkin-one"
-file_path = "C://Users//buchh//OneDrive/Desktop//"+file_name+".jsonl"
-user = "Alex"
+file_name = "entity_checkin_one_download_6May21"
+
+file_path = "/mnt/c/users/elle/documents/climatemind/climatemind-nlp/utils/database_download/"+file_name+".jsonl"
+
+user = "ElleGettingAnswers"
 data = srsly.read_jsonl(file_path)
 
 
 def get_answers(username):
-    f = open(username + "_answers.jsonl", "w")
+    checkuser = []
+    newfilename = username + "_answers.jsonl"
+    f = open(newfilename, "w")
     for entry in data:
         user_file = entry["_session_id"].replace("entity_checkin_one-", "")
         if user_file == username:
+            checkuser.append("Username found: " + username)
             ans = json.dumps(entry) + '\n'
             f.write(ans)
     f.close()
-
+    if checkuser:
+        print(checkuser[0])
+    else:
+        print("User not found")
 
 if not user:
     print("No username specified")
