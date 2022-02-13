1a331544ffe938e7f9cdb6c1b91599608f679994
Himesh <itsmee257@gmail.com>
2022-02-13 16:46:24 -0500

    updates

--- utils/model/data_setup/custom_bert_data_setup.py
+++ utils/model/data_setup/custom_bert_data_setup.py
@@ -1,30 +1,40 @@
 import srsly
+import pandas as pd
 
 file_name_answers = "checkin_answers"
 file_path_answers = "C://Users//buchh//OneDrive/Desktop//"+file_name_answers+".jsonl"
 data = srsly.read_jsonl(file_path_answers)
-final_sent = []
+sent_dict = []
+count = 1
 
 for entry in data:
     if "text" in entry:
         text = entry["text"]
-    else:
-        text = ""
-        throw("NO 'text' field encountered! This field is necessary for the rest of the script to work! Please fix this and then run this script.")
 
     label_arr = []
     label_tup = ()
     if entry['answer'] == "accept":
         for relation in entry['spans']:
+            sent_dict_tmp = {}
+            sent_dict_tmp["Sentence #"] = "Sentence " + str(count)
             if ("label" in relation) and ("start" in relation) and ("end" in relation):
+                sent_dict_tmp["text"] = text
                 child_span_start = relation["start"]
                 child_span_end = relation["end"]
                 word = text[child_span_start:child_span_end]
                 if relation["label"] == "base":
-                    tmp_tuple = (child_span_start, child_span_end, relation["label"])
-                    label_arr.append(tmp_tuple)
+                    sent_dict_tmp["word"] = text[child_span_start: child_span_end]
+                    sent_dict_tmp["tag"] = relation["label"]
+                else:
+                    sent_dict_tmp["word"] = text[child_span_start: child_span_end]
+                    sent_dict_tmp["tag"] = "O"
+            else:
+                sent_dict_tmp["word"] = text[child_span_start: child_span_end]
+                sent_dict_tmp["tag"] = "O"
+            sent_dict.append(sent_dict_tmp)
+    count += 1
 
-    label_tup = (str(text), {"entities": label_arr})
-    final_sent.append(label_tup)
+print(sent_dict)
 
-print(final_sent)
+df = pd.DataFrame.from_dict(sent_dict) 
+df.to_csv (r'custom_ent_bert_data.csv', index = False, header=True)
